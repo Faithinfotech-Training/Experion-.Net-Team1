867a6254ebb9eb05ebbcff89a2e5439799c0a4ea
chandramouli <chandramouli.sharma@experionglobal.com>
2021-11-30 06:28:49 +0530

    course enquiry service code

--- src/app/cenquiries/cenquiry-list/cenquiry-list.component.ts
+++ src/app/cenquiries/cenquiry-list/cenquiry-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
+import { Cenquiry } from 'src/app/shared/cenquiry';
+import { CenquiryService } from 'src/app/shared/cenquiry.service';
 
 @Component({
   selector: 'app-cenquiry-list',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cenquiry-list.component.css']
 })
 export class CenquiryListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  //assign default page
+  page: number=1;
+  filter: string;
+  
+    constructor(public cenquiryService:CenquiryService, private router:Router) { }
+  
+    ngOnInit(): void {
+      //get all course enquiry through service
+      this.cenquiryService.bindListCEnquiry();
+    }
+    //populate form by clicking the column fields
+    populateForm(cenquiry: Cenquiry)
+    {
+      console.log(cenquiry);
+      //date format
+      var datePipe= new DatePipe("en-UK");
+      let formatedDate:any=datePipe.transform(cenquiry.EnquiryDate,'yyyy-MM-dd');
+      cenquiry.EnquiryDate=formatedDate;
+      this.cenquiryService.formData=Object.assign({},cenquiry);
+    }
+   
+      //update course enquiry
+      updateCEnquiry(CEnquiryId: number)
+    {
+      console.log(CEnquiryId);
+      this.router.navigate(['cenquiry',CEnquiryId]);
+    }
   }
-
-}
+  
--- src/app/shared/cenquiry.ts
+++ src/app/shared/cenquiry.ts
@@ -1,2 +1,7 @@
 export class Cenquiry {
+    CourseEnquiryId:number;
+    Description:string='';
+    EnquiryDate:Date=new Date;
+    CourseId:number;
+    Email:string='';
 }
